af5deaad9391c411ef04033d3e6db07d40edadcb
cronokun <crono.neko@gmail.com>
2024-11-29 13:00:40 +0300

    Day 24, part 2: Never ever tell me the odds

--- lib/advent_of_code/24_never_tell_odds.ex
+++ lib/advent_of_code/24_never_tell_odds.ex
@@ -86,6 +86,58 @@ defmodule AdventOfCode.NeverTellOdds do
 
   Considering only the X and Y axes, check all pairs of hailstones' future paths for intersections.
   **How many of these intersections occur within the test area?**
+
+  ## Part Two
+
+  Upon further analysis, it doesn't seem like any hailstones will naturally collide. It's up to
+  you to fix that!
+
+  You find a rock on the ground nearby. While it seems extremely unlikely, if you throw it just
+  right, you should be able to hit every hailstone in a single throw!
+
+  You can use the probably-magical winds to reach any integer position you like and to propel the
+  rock at any integer velocity. Now including the Z axis in your calculations, if you throw the
+  rock at time 0, where do you need to be so that the rock perfectly collides with every
+  hailstone? Due to probably-magical inertia, the rock won't slow down or change direction when
+  it collides with a hailstone.
+
+  In the example above, you can achieve this by moving to position 24, 13, 10 and throwing the
+  rock at velocity -3, 1, 2. If you do this, you will hit every hailstone as follows:
+
+  - Hailstone: 19, 13, 30 @ -2, 1, -2
+    Collision time: 5
+    Collision position: 9, 18, 20
+
+  - Hailstone: 18, 19, 22 @ -1, -1, -2
+    Collision time: 3
+    Collision position: 15, 16, 16
+
+  - Hailstone: 20, 25, 34 @ -2, -2, -4
+    Collision time: 4
+    Collision position: 12, 17, 18
+
+  - Hailstone: 12, 31, 28 @ -1, -2, -1
+    Collision time: 6
+    Collision position: 6, 19, 22
+
+  - Hailstone: 20, 19, 15 @ 1, -5, -3
+    Collision time: 1
+    Collision position: 21, 14, 12
+
+  Above, each hailstone is identified by its initial position and its velocity. Then, the time
+  and position of that hailstone's collision with your rock are given.
+
+  After 1 nanosecond, the rock has exactly the same position as one of the hailstones,
+  obliterating it into ice dust! Another hailstone is smashed to bits two nanoseconds after that.
+  After a total of 6 nanoseconds, all of the hailstones have been destroyed.
+
+  So, at time 0, the rock needs to be at X position 24, Y position 13, and Z position 10. Adding
+  these three coordinates together produces 47. (Don't add any coordinates from the rock's
+  velocity.)
+
+  Determine the exact position and velocity the rock needs to have at time 0 so that it perfectly
+  collides with every hailstone. What do you get if you add up the X, Y, and Z coordinates of
+  that initial position?
   """
 
   @doc "Number of hailstone intersections that occur withing test area (ignoring Z axis)."
@@ -135,6 +187,52 @@ defmodule AdventOfCode.NeverTellOdds do
     y_match = if vy > 0, do: py > y, else: py < y
     x_match and y_match
   end
+
+  # ---- Part 2 --------
+
+  @doc "Returns sum of coordinates of initial stone position."
+  def final_answer(input) do
+    [h0 | _] = hs = parse(input)
+    [_, h1, h2, h3] = center_on_hailstone(hs, h0)
+    n = find_plane(h1)
+    i2 = line_plane_intersection(h2, n)
+    i3 = line_plane_intersection(h3, n)
+
+    relative_rock_position(i2, i3)
+    |> absolute_rock_position(h0)
+    |> Enum.sum()
+  end
+
+  defp center_on_hailstone(hs, [x0, y0, z0, vx0, vy0, vz0]) do
+    hs
+    |> Enum.take(4)
+    |> Enum.map(fn [x, y, z, vx, vy, vz] ->
+      [x - x0, y - y0, z - z0, vx - vx0, vy - vy0, vz - vz0]
+    end)
+  end
+
+  defp find_plane([x, y, z, vx, vy, vz]) do
+    [x1, y1, z1] = [x + vx, y + vy, z + vz]
+    [y * z1 - z * y1, z * x1 - x * z1, x * y1 - y * x1]
+  end
+
+  defp line_plane_intersection([x, y, z, vx, vy, vz], [nx, ny, nz]) do
+    a = -x * nx - y * ny - z * nz
+    b = vx * nx + vy * ny + vz * nz
+    t = div(a, b)
+    p = [x + vx * t, y + vy * t, z + vz * t]
+    {p, t}
+  end
+
+  defp relative_rock_position({[x2, y2, z2], t2}, {[x3, y3, z3], t3}) do
+    dt = t2 - t3
+    # Rock direction:
+    [dx, dy, dz] = [div(x2 - x3, dt), div(y2 - y3, dt), div(z2 - z3, dt)]
+    # Rock position:
+    [x2 - dx * t2, y2 - dy * t2, z2 - dz * t2]
+  end
+
+  defp absolute_rock_position([x, y, z], [x0, y0, z0, _, _, _]), do: [x0 + x, y0 + y, z0 + z]
 
   # ---- Parser --------
 
--- test/advent_of_code/24_never_tell_odds_test.exs
+++ test/advent_of_code/24_never_tell_odds_test.exs
@@ -1,5 +1,5 @@
 defmodule AdventOfCode4NeverTellOddsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   import AdventOfCode.NeverTellOdds
 
@@ -17,11 +17,27 @@ defmodule AdventOfCode4NeverTellOddsTest do
            ) == 2
   end
 
-  test ".answer/1 with test input" do
+  test ".final_answer/1 returns sum of coordinates of initial stone position" do
+    assert final_answer(~S"""
+           19, 13, 30 @ -2,  1, -2
+           18, 19, 22 @ -1, -1, -2
+           20, 25, 34 @ -2, -2, -4
+           12, 31, 28 @ -1, -2, -1
+           20, 19, 15 @  1, -5, -3
+           """) == 47
+  end
+
+  @input_data File.read!("priv/24_hailstone_notes")
+
+  test "Day 24, part 1" do
     assert answer(
-             File.read!("priv/24_hailstone_notes"),
+             @input_data,
              200_000_000_000_000,
              400_000_000_000_000
            ) == 11_995
+  end
+
+  test "Day 24, part 2" do
+    assert final_answer(@input_data) == 983_620_716_335_751
   end
 end
